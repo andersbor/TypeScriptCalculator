6debd11c1484aa70bc79977f435cf7292a924e13
Anders Børjesson <anbo@zealand.dk>
2020-02-20 13:51:17 +0100

    Update index.ts
    added few types

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,11 +4,11 @@ calculateButon.addEventListener("click", calculate);
 function calculate(): void {
     let input1: HTMLInputElement = <HTMLInputElement>document.getElementById("number1");
     let input2: HTMLInputElement = <HTMLInputElement>document.getElementById("number2");
-    let number1Str = input1.value;
-    let number2Str = input2.value;
+    let number1Str: string = input1.value;
+    let number2Str: string = input2.value;
 
-    let number1 = Number(number1Str);
-    let number2 = Number(number2Str);
+    let number1: number = Number(number1Str);
+    let number2: number = Number(number2Str);
 
     let operationElement: HTMLSelectElement = <HTMLSelectElement>document.getElementById("operation");
     let operation: string = operationElement.value;
